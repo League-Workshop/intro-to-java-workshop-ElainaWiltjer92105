3a6425ad18a7c87620a74f653db5e21fcd2da654
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-17 13:27:14 -0700

    DragonFight

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,17 @@
 package section4;
 
-public class QuizGame {
+import javax.swing.JOptionPane;
+
+public class QuizGame { 
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int scoreKeeper =0;
 		// 2.  Ask the user a question 
-		
+		String Question = JOptionPane.showInputDialog("Do you like HomeWork?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (Question.equalsIgnoreCase(" no ")) {
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
